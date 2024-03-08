69b5a0713c356e0c111c79c28018b828079ee227
Youssef-At <72509411+Youssef-At@users.noreply.github.com>
2024-03-08 02:01:55 +0100

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,12 +3,18 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  // floors getter
   get floors() {
     return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
